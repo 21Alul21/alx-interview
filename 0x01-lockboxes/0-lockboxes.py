6df9633cd25b172e3bd7269a825407760ef09df6
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2024-01-15 00:05:28 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,10 +9,21 @@ def canUnlockAll(boxes):
     implementation
     """
 
+    # Set to keep track of opened boxes
     opened_boxes = {0}
-    ranges = set(range(1, len(boxes) + 1))
-    for box in boxes:
-        for key in boxes:
-            if key in ranges:
-                opened_boxes.add(key)
+    
+    # Set to keep track of keys that need to be explored
+    keys_to_explore = set(boxes[0])
+
+    # Iterate through keys to explore
+    while keys_to_explore:
+        key = keys_to_explore.pop()
+        
+        # Check if the key opens a box
+        if key < len(boxes) and key not in opened_boxes:
+            opened_boxes.add(key)
+            keys_to_explore.update(boxes[key])
+
+    # Check if all boxes have been opened
     return len(opened_boxes) == len(boxes)
+
