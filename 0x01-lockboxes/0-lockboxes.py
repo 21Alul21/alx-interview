b5549c3e640985992e00af8301bceafc9d2b83dc
root <augustine.alul@yahoo.com>
2024-01-14 09:47:50 +0000

    lockboxes solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/use/bin/python3
+"""Module containing the solution
+to the lockboxes algorithmic task
+"""
+
+
+def canUnlockAll(boxes) -> bool:
+    """the method for the
+    implementation
+    """
+
+    opened_boxes = {0}
+    ranges = set(range(1, len(boxes)))
+    for box in boxes:
+        for key in boxes:
+            if key in ranges:
+                opened_boxes.add(key)
+    return len(opened_boxes) == len(boxes)
+
